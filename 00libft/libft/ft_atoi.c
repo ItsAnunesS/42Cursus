fe7f3e4c52aba4eebc1ffe65b7e8b47d4443bb20
André Nunes <hello@anuness.dev>
2021-03-05 04:14:11 +0000

    Backup Save

--- 00libft/libft/ft_atoi.c
+++ 00libft/libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: anunes-d <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/05 01:00:25 by anunes-d          #+#    #+#             */
-/*   Updated: 2021/03/05 01:02:04 by anunes-d         ###   ########.fr       */
+/*   Updated: 2021/03/05 03:34:52 by anunes-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,29 @@
 
 int	ft_atoi(const char *nptr)
 {
+	int			sign;
+	long long	res;
 
+	sign = 1;
+	res = 0;
+	while (*nptr == '\t' || *nptr == '\n' || *nptr == '\v' ||
+	*nptr == '\f' || *nptr == '\r' || *nptr == ' ')
+		nptr++;
+	if (*nptr == '-')
+	{
+		sign = -1;
+		nptr++;
+	}
+	else if (*nptr == '+')
+		nptr++;
+	while (*nptr >= '0' && *nptr <= '9')
+	{
+		res *= 10;
+		res += (sign * (*(nptr++) - '0'));
+		if (res > 2147483647)
+			return (-1);
+		if (res < -2147483648)
+			return (0);
+	}
+	return (res);
 }
---
+++ 00libft/libft/ft_strdup.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anunes-d <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/05 03:59:24 by anunes-d          #+#    #+#             */
+/*   Updated: 2021/03/05 04:12:11 by anunes-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strdup(const char *s)
+{
+	int s_size;
+    static char *dup;
+    char *dup_offset;
+
+	s_size = ft_strlen(s);
+	dup = (char *)malloc(sizeof(char)*s_size+1);
+	if(dup == NULL)
+        return((char *)NULL);
+	dup_offset = dup;
+
+	while(*s)
+    {
+        *dup_offset = *s;
+        dup_offset++;
+        s++;
+    }
+    *dup_offset = '\0';
+
+    return(dup);
+}
