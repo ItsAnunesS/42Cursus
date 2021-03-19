edf1a3e6c6ba69b7473c694e697f6fa53983ac6e
André Nunes <hello@anuness.dev>
2021-03-19 15:45:04 +0000

    Operação Falcão

---
+++ 00libft/libft/ft_lstmap.c
@@ -0,0 +1,22 @@
+
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new;
+	t_list	*first;
+
+	first = NULL;
+	while (lst)
+	{
+		if (!(new = ft_lstnew(f(lst->content))))
+		{
+			ft_lstclear(&first, del);
+			return (0);
+		}
+		ft_lstadd_back(&first, new);
+		lst = lst->next;
+	}
+	return (first);
+}
